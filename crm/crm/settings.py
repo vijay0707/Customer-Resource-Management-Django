ea50b80f57aeedeb8afd08d5e7f1297d4927634c
Vijay Saran <59793973+vijay0707@users.noreply.github.com>
2021-09-09 14:31:00 +0530

    Update settings.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'flaskform00@gmail.com'
-EMAIL_HOST_PASSWORD = 'flasktest123'
+EMAIL_HOST_USER = 'enter email id'
+EMAIL_HOST_PASSWORD = 'enter password'
